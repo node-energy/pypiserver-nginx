5cc4d5fcd553a9562cd1b5a66ba8784ac5fc7143
Lars Rinn <22575361+larsrinn@users.noreply.github.com>
2022-07-26 07:56:18 +0200

    iterate localtunnel

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -11,7 +11,7 @@ domains=(pypi.node.energy lt.node.energy)
 rsa_key_size=4096
 data_path="./data/certbot"
 read -p "Please enter your e-mail-address " email
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
